65787ab8fa5a33e2d4d734db8cebaf20bd7080a7
prasunaa4 <prasunaa4@gmail.com>
2016-11-02 15:07:54 -0500

    Update Exercise1.java

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -5,36 +5,32 @@ package com.elsevier.education;
 TODO: Make this class immutable.
 
 */
-public class Exercise1 {
+//Class need to be final to avoid inheritance.
+public final class Exercise1 {
 
 	public static class Person {
-		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
-		
-		public Person() {
-		}
+		// class properties need to be final 
+		private final Set<String> phoneNumbers;
+		private final  String firstName;
+		private final String lastName;
+		// initializing class properties through constructor.
+		public Person(Set<String> phoneNumbers,String lastName,String firstName) {
 
+			this.phoneNumbers =phoneNumbers;
+			this.lastName =lastName;
+			this.firstName =firstName;
+		}
+// no setters for class properties to avoid modifications.
 		public Set<String> getPhoneNumbers() {
 			return phoneNumbers;
 		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
-			phoneNumbers = newPhoneNumbers;
-		}
-		
+				
 		public String getFirstName() {
 			return firstName;
 		}
-		public void setFirstName(String newName) {
-			firstName = newName;
-		}
-		
+			
 		public String getLastName() {
 			return lastName;
 		}
-		public void setLastName(String newName) {
-			lastName = newName;
-		}
-	}
+		
 }
